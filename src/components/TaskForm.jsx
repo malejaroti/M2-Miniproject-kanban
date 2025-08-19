08787dfb5099498d7050a05b7f1d3242c1d3524a
Carlos Canet <carlos@carlosca.net>
2025-08-19 21:40:15 +0200

    fix: better styling and avoid adding blank tasks

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,6 +1,6 @@
 import { Datepicker } from "flowbite-react";
 import { useState } from "react";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 function TaskForm({ data, onSubmit, actionText, assignees, priorities, statuses }) {
   const [formData, setFormData] = useState({
@@ -40,42 +40,48 @@ function TaskForm({ data, onSubmit, actionText, assignees, priorities, statuses 
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className="border-5 flex flex-col p-5 mt-5">
+      <form onSubmit={handleSubmit} className="border-5 flex flex-col p-5 mt-5 ">
         <div className="text-sky-500">
-          <label className="flex items-center flex-none my-5 w-1/2 font-bold">
+          <label className="flex items-center flex-none my-5 w-1/2 font-bold text-lg">
             <p className="w-1/6 text-end mr-5">Task title</p>
-            <input className="w-full font-medium text-sky-700" name="title" type="text" placeholder="Title" value={formData.title} onChange={handleChange} />
+            <input className="w-full font-medium text-sky-700 bg-blue-200 p-2 rounded-sm" name="title" type="text" placeholder="Title" value={formData.title} onChange={handleChange} required/>
           </label>
-          <label className="flex items-center flex-none my-5 w-1/2 font-bold">
+          <label className="flex items-center flex-none my-5 w-1/2 font-bold text-lg">
             <p className="w-1/6 text-end mr-5">Description</p>
-            <textarea className="w-full font-medium text-sky-700" name="description" type="text" placeholder="Description" value={formData.description} onChange={handleChange} />
+            <textarea className="w-full font-medium text-sky-700 bg-blue-200 p-2 rounded-sm" name="description" type="text" placeholder="Description" value={formData.description} onChange={handleChange} />
           </label>
-          <label className="flex items-center flex-none my-5 w-1/2 font-bold">
+          <label className="flex items-center flex-none my-5 w-1/2 font-bold text-lg">
             <p className="w-1/6 text-end mr-5">Assignee</p>
-            <select className="w-full font-medium text-sky-700" name="assignee" type="text" placeholder="Assignee name" value={formData.assignee} onChange={handleChange}>
+            <select className="w-full font-medium text-sky-700 bg-blue-200 p-2 rounded-sm" name="assignee" type="text" placeholder="Assignee name" value={formData.assignee} onChange={handleChange} >
+              <option value="" placeholder="Assignee"></option>
               {Array.from(assignees).map(assignee => <option value={assignee} key={assignee}>{assignee}</option>)}
             </select>
           </label>
-          <label className="flex items-center flex-none my-5 w-1/2 font-bold">
+          <label className="flex items-center flex-none my-5 w-1/2 font-bold text-lg">
             <p className="w-1/6 text-end mr-5">Status</p>
-            <select className="w-full font-medium text-sky-700" name="status" type="text" placeholder="Status" value={formData.status} onChange={handleChange} >
+            <select className="w-full font-medium text-sky-700 bg-blue-200 p-2 rounded-sm" name="status" type="text" placeholder="Status" value={formData.status} onChange={handleChange} >
               {Array.from(statuses).map(status => <option value={status} key={status}>{status}</option>)}
             </select>
           </label>
-          <label className="flex items-center flex-none my-5 w-1/2 font-bold">
+          <label className="flex items-center flex-none my-5 w-1/2 font-bold text-lg">
             <p className="w-1/6 text-end mr-5">Priority</p>
-            <select className="w-full font-medium text-sky-700" name="priority" type="text" placeholder="Priority" value={formData.priority} onChange={handleChange} >
+            <select className="w-full font-medium text-sky-700 bg-blue-200 p-2 rounded-sm" name="priority" type="text" placeholder="Priority" value={formData.priority} onChange={handleChange} >
               {Array.from(priorities).map(priority => <option value={priority} key={priority} >{priority}</option>)}
             </select>
           </label>
-          <label className="flex items-center flex-none my-5 w-1/2 font-bold">
+          <label className="flex items-center flex-none my-5 w-1/2 font-bold text-lg">
             <p className="w-1/6 text-end mr-5">Due Date</p>
-            <Datepicker value={formData.dueDate} onChange={handleChange} />
+            <Datepicker value={formData.dueDate} onChange={handleChange} required />
           </label>
 
           <button className="bg-sky-800 m-5 p-5 rounded-t-lg text-sky-400 cursor-pointer shadow-md shadow-sky-600 hover:relative hover:top-0.5 hover:left-0.5" type="submit">
             {actionText}
           </button>
+          <Link to="/">
+          <button className="bg-sky-950 w-25 m-5 p-5 rounded-t-lg text-sky-400 cursor-pointer shadow-md shadow-sky-900 hover:relative hover:top-0.5 hover:left-0.5" type="submit">
+            Back
+          </button>
+          </Link>
         </div>
       </form>
     </div>
